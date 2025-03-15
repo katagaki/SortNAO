348ef51da3ce52ac3e8acb982e457cfbbe996079
katagaki <68231035+katagaki@users.noreply.github.com>
2025-03-15 11:13:22 +0900

    Initial Commit

---
+++ SortNAO/SortNAOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortNAOApp.swift
+//  SortNAO
+//
+//  Created by シン・ジャスティン on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct SortNAOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
